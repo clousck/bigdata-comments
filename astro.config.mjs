520b930d837a75d7614a79430ebf4e5324de3579
Clousck <victorecheverriasoto@gmail.com>
2025-01-04 18:51:10 -0500

    Fix mistake config astro.conf.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default {
-    site: 'https://clousck.github.io/bigdata-comments/',
-    base: '/bigdata-comments/', // Asegúrate de reemplazar <NOMBRE_DEL_REPOSITORIO> por el nombre de tu repositorio
+    site: 'https://clousck.github.io',
+    base: '/bigdata-comments/', 
   };
   
